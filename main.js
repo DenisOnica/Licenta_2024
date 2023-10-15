4703830fdb217b20c7e7be8abb33922dd8f37d9c
Denis Onica <denis.onica97@gmail.com>
2023-10-15 14:48:06 +0300

    commit 4

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ import "./style.css";
 
 import viteLogo from "/vite.svg";
 
-document.querySelector("#app").innerHTML = `
+document.querySelector("#app").innerHTML += `
   <div>
    
-    <h1>Cine citeste muie primeste</h1>
-    
+    <h1>Hello</h1>
+
     
       
   </div>
