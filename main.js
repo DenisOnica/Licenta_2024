c45aebdf2a21d103610f1d4b59b179b334709fab
Denis Onica <denis.onica97@gmail.com>
2024-03-23 18:35:54 +0200

    test

--- main.js
+++ main.js
@@ -2,12 +2,17 @@ import "./style.css";
 
 import viteLogo from "/vite.svg";
 
-document.querySelector("#app").innerHTML += `
-  <div>
-   
-    <h1>Hello</h1>
-
-    
-      
-  </div>
-`;
+window.addEventListener("load", async () => {
+  const response = await fetch(
+    `https://652bdb8ed0d1df5273eecf9b.mockapi.io/products`
+  );
+  const products = await response.json();
+  const productDiv = document.querySelector("#products");
+  let productsHTML = "";
+  for (let i = 0; i < products.length; i++) {
+    productsHTML += `<div class="card">
+      <img src=${products[i].imageURL}/>
+      </div>`;
+  }
+  productDiv.innerHTML = productsHTML;
+});
